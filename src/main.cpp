f0fb235e0529559c12ac2b4ab61cef86d2062a21
sh123 <sh123@users.noreply.github.com>
2025-02-12 19:06:21 +0200

    Code update

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,42 @@
 #include <arduino-timer.h>
 #include <Stepper.h>
 
-#define CFG_STEPPER_RPM     5
-#define CFG_STEPPER_STEPS   200
-#define CFG_STEPPER_STEP    1
-#define CFG_STEPPER_WAIT_MS 60L*1000L/CFG_STEPPER_STEPS/CFG_STEPPER_RPM     
+#define CFG_STEPPER_RPM         5
+#define CFG_STEPPER_STEPS       200
+#define CFG_STEPPER_STEP        1
+#define CFG_STEPPER_WAIT_MS     60L*1000L/CFG_STEPPER_STEPS/CFG_STEPPER_RPM     
+#define CFG_STEPPER_PIN_M0      4
+#define CFG_STEPPER_PIN_M1      5
+#define CFG_STEPPER_PIN_M2      6
+#define CFG_STEPPER_PIN_M3      7
 
-#define CFG_STEPPER_PIN_M0  4
-#define CFG_STEPPER_PIN_M1  5
-#define CFG_STEPPER_PIN_M2  6
-#define CFG_STEPPER_PIN_M3  7
-#define CFG_LED_PIN         8
-#define CFG_HOTEND_PIN      A0
-#define CFG_RUNOUT_PIN      2
-#define CFG_THERMISTOR_PIN  A2
+#define CFG_LED_PIN             8
+#define CFG_HOTEND_PIN          3
+#define CFG_RUNOUT_PIN          2
+
+#define CFG_THERMISTOR_PIN      A2
+#define CFG_THERMISTOR_WAIT_MS  2L*1000L
+#define CFG_THERMISTOR_R        10000
 
 Stepper stepper_(CFG_STEPPER_STEPS, CFG_STEPPER_PIN_M0, 
     CFG_STEPPER_PIN_M1, CFG_STEPPER_PIN_M2, CFG_STEPPER_PIN_M3);
 
-Timer<2, millis> timer_;
+Timer<3, millis> timer_;
 
 volatile bool hasFilament_ = false;
 
 void stepperRelease() {
-    digitalWrite(CFG_STEPPER_PIN_M0, LOW);  
-    digitalWrite(CFG_STEPPER_PIN_M1, LOW);  
-    digitalWrite(CFG_STEPPER_PIN_M2, LOW);  
-    digitalWrite(CFG_STEPPER_PIN_M3, LOW);  
+    digitalWrite(CFG_STEPPER_PIN_M0, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M1, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M2, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M3, LOW); 
+}
+
+bool stepperStep(void *arg) {
+    if (hasFilament_) {
+        stepper_.step(CFG_STEPPER_STEP);
+    }
+    return true;
 }
 
 void runoutTriggered() {
@@ -42,17 +52,23 @@ void runoutTriggered() {
     }
 }
 
-bool debugPrint(void *arg) {
-    uint8_t t = analogRead(CFG_THERMISTOR_PIN);
-    Serial.print(t, HEX); Serial.println();
-    digitalWrite(CFG_LED_PIN, !digitalRead(CFG_LED_PIN));
+double thermistorRead() {
+    double c1 = 0.8438162826e-03, c2 = 2.059601750e-04, c3 = 0.8615484887e-07;
+    double level = (double)analogRead(CFG_THERMISTOR_PIN);
+    double levelR = CFG_THERMISTOR_R * (1023.0 / level - 1.0);
+    double levelLogR = log(levelR);
+    double tempKelvin = (1.0 / (c1 + c2 * levelLogR + c3 * levelLogR * levelLogR * levelLogR)); 
+    return tempKelvin - 273.15;
+}
+
+bool thermistorProcess(void *arg) {
+    double t = thermistorRead();
+    Serial.print(t); Serial.println();
     return true;
 }
 
-bool stepperStep(void *arg) {
-    if (hasFilament_) {
-        stepper_.step(CFG_STEPPER_STEP);
-    }
+bool debugPrint(void *arg) {
+    digitalWrite(CFG_LED_PIN, !digitalRead(CFG_LED_PIN));
     return true;
 }
 
@@ -60,14 +76,18 @@ void setup() {
     pinMode(CFG_LED_PIN, OUTPUT);
     pinMode(CFG_HOTEND_PIN, OUTPUT);
     pinMode(CFG_RUNOUT_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(CFG_RUNOUT_PIN), runoutTriggered, CHANGE);
+
     stepper_.setSpeed(CFG_STEPPER_RPM);
+
+    timer_.every(1000, debugPrint);
+    timer_.every(CFG_STEPPER_WAIT_MS, stepperStep);
+    timer_.every(CFG_THERMISTOR_WAIT_MS, thermistorProcess);
+
+    attachInterrupt(digitalPinToInterrupt(CFG_RUNOUT_PIN), runoutTriggered, CHANGE);
 
     Serial.begin(115200);
     while (!Serial);
 
-    timer_.every(1000, debugPrint);
-    timer_.every(CFG_STEPPER_WAIT_MS, stepperStep);
     Serial.println(F("Started"));
 }
 

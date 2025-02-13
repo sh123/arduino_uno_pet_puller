8c5f7103da74d6c55cbde09700270972d25fa71e
sh123 <sh123@users.noreply.github.com>
2025-02-13 09:36:42 +0200

    Code update

--- src/main.cpp
+++ src/main.cpp
@@ -39,18 +39,23 @@ Stepper stepper_(CFG_STEPPER_STEPS, CFG_STEPPER_PIN_M0,
 PID pid_(&pidInput_, &pidOutput_, &pidSetpoint_, CFG_PID_KP, 
     CFG_PID_KI, CFG_PID_KD, DIRECT);
 
-void stepperRelease() {
-    digitalWrite(CFG_STEPPER_PIN_M0, LOW); 
-    digitalWrite(CFG_STEPPER_PIN_M1, LOW); 
-    digitalWrite(CFG_STEPPER_PIN_M2, LOW); 
-    digitalWrite(CFG_STEPPER_PIN_M3, LOW); 
-}
-
 bool stepperStep(void *arg) {
     if (hasFilament_) {
         stepper_.step(CFG_STEPPER_STEP);
     }
     return true;
+}
+
+void stepperInitialize() {
+    stepper_.setSpeed(CFG_STEPPER_RPM);
+    timer_.every(CFG_STEPPER_RUN_MS, stepperStep);
+}
+
+void stepperRelease() {
+    digitalWrite(CFG_STEPPER_PIN_M0, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M1, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M2, LOW); 
+    digitalWrite(CFG_STEPPER_PIN_M3, LOW); 
 }
 
 void runoutTriggered() {
@@ -64,6 +69,11 @@ void runoutTriggered() {
     }
 }
 
+void runoutInitialize() {
+    pinMode(CFG_RUNOUT_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(CFG_RUNOUT_PIN), runoutTriggered, CHANGE);
+}
+
 double thermistorRead() {
     double c1 = 0.8438162826e-03, c2 = 2.059601750e-04, c3 = 0.8615484887e-07;
     double level = (double)analogRead(CFG_THERMISTOR_PIN);
@@ -72,6 +82,7 @@ double thermistorRead() {
     double tempKelvin = (1.0 / (c1 + c2 * levelLogR + c3 * levelLogR * levelLogR * levelLogR)); 
     return tempKelvin - 273.15;
 }
+
 
 bool hotendProcess(void *arg) {
     pidInput_ = thermistorRead();
@@ -90,23 +101,21 @@ bool hotendProcess(void *arg) {
     return true;
 }
 
-void setup() {
+void hotendInitialize() {
     pinMode(CFG_LED_PIN, OUTPUT);
     pinMode(CFG_HOTEND_PIN, OUTPUT);
-    pinMode(CFG_RUNOUT_PIN, INPUT);
-
-    stepper_.setSpeed(CFG_STEPPER_RPM);
-
     analogWrite(CFG_HOTEND_PIN, 0);
-
     pid_.SetTunings(CFG_PID_KP, CFG_PID_KI, CFG_PID_KD);
     pid_.SetOutputLimits(0, CFG_PID_MAX_OUTPUT);
     pid_.SetMode(AUTOMATIC);
-
-    timer_.every(CFG_STEPPER_RUN_MS, stepperStep);
     timer_.every(CFG_THERMISTOR_RUN_MS, hotendProcess);
+}
 
-    attachInterrupt(digitalPinToInterrupt(CFG_RUNOUT_PIN), runoutTriggered, CHANGE);
+void setup() {
+
+    hotendInitialize();
+    stepperInitialize();
+    runoutInitialize();
 
     Serial.begin(115200);
     while (!Serial);
